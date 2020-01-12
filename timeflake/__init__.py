557198c65531aa8c96f18ba38aa00077df2e64f8
Anthony Najjar Simon <anthonynsimon@users.noreply.github.com>
2020-01-12 22:01:36 +0100

    Expose next() counter method in global instance

--- timeflake/__init__.py
+++ timeflake/__init__.py
@@ -1,9 +1,10 @@
-from timeflake.flake import Timeflake
-
 __version__ = "0.1.3"
+
+from timeflake.flake import Timeflake
 
 # Global instance
 _global_instance = Timeflake()
 EPOCH = _global_instance.epoch
 random = _global_instance.random
+next = _global_instance.next
 parse = _global_instance.parse
