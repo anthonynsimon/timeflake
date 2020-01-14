e95f5be543df09dc2e87db824eea89c2c88ce813
Anthony Najjar Simon <anthonynsimon@users.noreply.github.com>
2020-01-14 23:53:45 +0100

    Move version into init

--- timeflake/__init__.py
+++ timeflake/__init__.py
@@ -1,5 +1,7 @@
 from timeflake.flake import DEFAULT_ALPHABET, DEFAULT_EPOCH, Timeflake
 
+__version__ = "0.2.0"
+
 __all__ = ["Timeflake", "DEFAULT_ALPHABET", "DEFAULT_EPOCH", "random", "parse"]
 
 _global_timeflake = Timeflake()
