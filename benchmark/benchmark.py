677e2498d18e9c363a5180bbe7cf7fb93169c883
Anthony Najjar Simon <anthonynsimon@users.noreply.github.com>
2020-01-15 20:00:17 +0100

    Fmt

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -3,22 +3,26 @@ import timeit
 
 def main():
     res = timeit.timeit(
-        "timeflake.parse(from_base54=value)", setup="import timeflake; value = timeflake.random().base54"
+        "timeflake.parse(from_base54=value)",
+        setup="import timeflake; value = timeflake.random().base54",
     )
     print(f"timeflake.parse(from_base54): {res}")
-    
+
     res = timeit.timeit(
-        "timeflake.parse(from_bytes=value)", setup="import timeflake; value = timeflake.random().bytes"
+        "timeflake.parse(from_bytes=value)",
+        setup="import timeflake; value = timeflake.random().bytes",
     )
     print(f"timeflake.parse(from_bytes): {res}")
-    
+
     res = timeit.timeit(
-        "timeflake.parse(from_hex=value)", setup="import timeflake; value = timeflake.random().hex"
+        "timeflake.parse(from_hex=value)",
+        setup="import timeflake; value = timeflake.random().hex",
     )
     print(f"timeflake.parse(from_hex): {res}")
 
     res = timeit.timeit(
-        "timeflake.parse(from_int=value)", setup="import timeflake; value = timeflake.random().int"
+        "timeflake.parse(from_int=value)",
+        setup="import timeflake; value = timeflake.random().int",
     )
     print(f"timeflake.parse(from_int): {res}")
 
