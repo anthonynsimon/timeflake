a72c4a569b6edb6f503defb881e3055e8ca4be0b
Anthony Najjar Simon <anthonynsimon@users.noreply.github.com>
2020-01-18 16:03:33 +0100

    Update inv tasks

--- tasks.py
+++ tasks.py
@@ -2,6 +2,11 @@ import invoke
 
 
 @invoke.task
+def fmt(ctx):
+    ctx.run("black .")
+
+
+@invoke.task
 def test(ctx):
     ctx.run("pytest -v -s")
 
@@ -12,11 +17,16 @@ def benchmark(ctx):
 
 
 @invoke.task
+def clean(ctx):
+    ctx.run("rm -rf dist")
+    ctx.run("python setup.py clean --all")
+
+
+@invoke.task(test, clean)
 def build(ctx):
-    ctx.run("python setup.py clean")
     ctx.run("python setup.py sdist bdist_wheel")
 
 
-@invoke.task
+@invoke.task(fmt, build)
 def publish(ctx):
     ctx.run("twine upload dist/*")
